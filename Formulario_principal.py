786d6b179687f12760018ab92862899c8997c9ff
BrayanMunoz96 <42417269+BrayanMunoz96@users.noreply.github.com>
2022-09-06 00:44:34 -0500

    Create Formulario_principal.py
    Inicio del programa en una función principal

---
+++ Formulario_principal.py
@@ -0,0 +1,9 @@
+def main():
+	print("Formulario para nuevos usuarios")
+	nombre = input("Ingrese su nombre completo")
+	edad = input("Ingresa también tu edad ")
+
+	print("-------------Procesando--------------")
+
+
+main()
