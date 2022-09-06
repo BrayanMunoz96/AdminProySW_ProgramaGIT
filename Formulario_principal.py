986ea6bead1f7826d9f583628b0b3661ee56db67
iFerxo18 <54253281+iFerxo18@users.noreply.github.com>
2022-09-06 00:53:00 -0500

    Update Formulario_principal.py

--- Formulario_principal.py
+++ Formulario_principal.py
@@ -1,9 +1,18 @@
 def main():
 	print("Formulario para nuevos usuarios")
+	
+	#Preguntas
 	nombre = input("Ingrese su nombre completo")
 	edad = input("Ingresa también tu edad ")
+	helado = input("Cual es tu sabor de helado favorito?")
 
 	print("-------------Procesando--------------")
-
+	
+	#Respuestas
+	print("Tu nombre es ", nombre)
+	print("\n")
+	print("Tienes {} años\n".format(edad))
+	print("Tu sabor de helado favorito es ", helado)
+	print("\n")
 
 main()
