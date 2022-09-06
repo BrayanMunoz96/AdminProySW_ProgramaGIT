4c33a9416621f37e7b481d2022a85770485ffb43
erickmtz97x <112738081+erickmtz97x@users.noreply.github.com>
2022-09-06 00:59:34 -0500

    Update Formulario_principal.py

--- Formulario_principal.py
+++ Formulario_principal.py
@@ -5,6 +5,7 @@ def main():
 	nombre = input("Ingrese su nombre completo")
 	edad = input("Ingresa también tu edad ")
 	helado = input("Cual es tu sabor de helado favorito?")
+	pelicula = input("¿Cual es tu pelicula favorita?")
 
 	print("-------------Procesando--------------")
 	
@@ -13,6 +14,8 @@ def main():
 	print("\n")
 	print("Tienes {} años\n".format(edad))
 	print("Tu sabor de helado favorito es ", helado)
+	print("\n")
+	print ("Mi pelicula favorita es ", pelicula)
 	print("\n")
 
 main()
